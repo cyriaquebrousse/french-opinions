e01e1fa9dc6f932faf06dc8e4a81a8a5fc4c0ec4
Cyriaque Brousse <cyriaque.brousse@epfl.ch>
2015-05-11 21:26:08 +0200

    libraries

---
+++ FrenchOpinions/lib/run_mallet.sh
@@ -0,0 +1,20 @@
+# run_mallet.sh
+# Use: sh run_mallet.sh language inputdir numtopics
+#      (language must be in lowercase two-letter form)
+
+# Parameters
+language="$1"
+inputdir="$2"
+numtopics="$3"
+
+malletfile="topics/.mallet"
+outputkeys="topics/keys.txt"
+outputcompo="topics/compo.txt"
+inferencer="topics/inferencer.ser"
+
+
+# Importing the corpus directory
+./mallet/bin/mallet import-dir --input "$inputdir" --output "$malletfile" --keep-sequence --token-regex "[\p{L}\p{M}]+" --remove-stopwords --stoplist-file ./mallet_stoplist_"$language".txt
+
+# Training the topic model
+./mallet/bin/mallet train-topics --input "$malletfile" --num-topics "$numtopics" --output-topic-keys "$outputkeys" --output-doc-topics "$outputcompo" --num-top-words 16 --optimize-interval 20
