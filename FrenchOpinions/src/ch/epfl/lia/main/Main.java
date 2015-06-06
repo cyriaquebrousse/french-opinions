901902a2ef44a2c01d24f1e0b0a30ac3b6c490c3
Cyriaque Brousse <cyriaque.brousse@epfl.ch>
2015-06-06 16:38:06 +0200

    remove unused method

--- FrenchOpinions/src/ch/epfl/lia/main/Main.java
+++ FrenchOpinions/src/ch/epfl/lia/main/Main.java
@@ -1,6 +1,4 @@
 package ch.epfl.lia.main;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -78,45 +76,5 @@ public class Main {
         /* Statistics */
         System.out.println(evalBuilder.build());
     }
-
-    private static void getMostProminentTopicsForArticles(Collection<Topic> topics) {
-        System.out.println("\n>> Most prominent topics per article:");
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("topics/compo.txt"));
-            String line;
-            
-            reader.readLine();
-            while ((line = reader.readLine()) != null) {
-                line = line.trim();
-                if (line.isEmpty()) {
-                    continue;
-                }
-                
-                String[] split = line.split("\\s+");
-                final int articleId = Integer.parseInt(split[1].substring(
-                        split[1].lastIndexOf('/') + 1, split[1].indexOf(".txt")));
-                List<Integer> mostProminentTopics = new ArrayList<>();
-                
-                for (int i = 2; i < split.length - 1; i += 2) {
-                    if (Double.parseDouble(split[i + 1]) >= 0.1) {
-                        mostProminentTopics.add(Integer.parseInt(split[i]));
-                    } else {
-                        break;
-                    }
-                }
-                
-                // FIXME incorrect
-                if (mostProminentTopics.isEmpty()) {
-                    mostProminentTopics.add(Integer.parseInt(split[2]));
-                }
-                
-                System.out.print("Article " + articleId + ": \t");
-                System.out.println(mostProminentTopics);
-            }
-            reader.close();
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    
 }
